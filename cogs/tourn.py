05e8a03a7df02aa00ae8de2424503dcb765ac823
NBZion <subtopewds0921@gmail.com>
2024-10-28 17:58:51 +0800

    Add Initial "Add Team" Command

--- cogs/tourn.py
+++ cogs/tourn.py
@@ -5,13 +5,35 @@ from discord.ext import commands
 
 
 ## Whitelisted Servers 
-whitelistedServers=[1286147910085513287]
+whitelistedServers=[1299326763628757053]
 
 
 
 class tourn(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.slash_command(guild_ids=whitelistedServers, description="Adds a Team for Ludopocalypse")
+    @commands.has_permissions(manage_messages=True)
+    async def add_team(ctx, teamName):
+        guild = ctx.guild()
+
+        organizerRole = ctx.guild.get_role()
+
+        teamRole = await guild.create_role(name=teamName)
+
+        team_category = await guild.create_category("TEAM | " + teamName)
+
+        await guild.create_text_channel(f"{teamName}-text", category=team_category)
+        await guild.create_voice_channel(f"{teamName}-voice", category=team_category)
+
+        # Set everyone to not see VC
+        await team_category.set_permissions(guild.default_role, view_channel=False)
+
+        await team_category.set_permissions(teamRole, view_channel=True, send_messages=True, connect=True)
+        await team_category.set_permissions(organizerRole, view_channel=True, send_messages=True, connect=True)
+
+        await ctx.respond("Succesfully created team '" + teamName + "'!") 
 
 
 
